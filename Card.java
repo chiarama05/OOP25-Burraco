5e7394551a2eefe108cf2ba01b780fedc0269eb3
Sofia <sofia.mascioli2@studio.unibo.it>
2026-01-21 11:37:09 +0100

    creation of Card class

---
+++ Card.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Card {
+    private String seed;
+    private String value;
+
+    public Card(String seed, String value){
+        this.seed=seed;
+        this.value=value;
+    }
+}
