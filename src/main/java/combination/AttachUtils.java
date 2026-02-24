1f04751b437b0a26519b37966ca7383884779ec9
Sofia <sofia.mascioli2@studio.unibo.it>
2026-02-24 15:20:26 +0100

    add attach logic for sets and straights

---
+++ src/main/java/combination/AttachUtils.java
@@ -0,0 +1,20 @@
+package combination;
+
+import java.util.List;
+import card.Card;
+
+public class AttachUtils {
+
+    public static boolean canAttach(List<Card> combination, Card newCard) {
+
+        if (combination == null || combination.isEmpty()) {
+            return false;
+        }
+
+        if (StraightUtils.isStraight(combination)) {
+            return StraightAttachUtils.canAttachToStraight(combination, newCard);
+        }
+
+        return SetAttachUtils.canAttachToSet(combination, newCard);
+    }
+}
