b8656ba7fd1ffea7a297fcb50d029dfd6434b951
Giorgia Ceccaroni <giorgia.ceccaroni@studio.unibo.it>
2026-02-26 19:34:40 +0100

    update1 to TableViewImpl

--- src/main/java/creating_table/TableViewImpl.java
+++ src/main/java/creating_table/TableViewImpl.java
@@ -1,7 +1,24 @@
 import javax.swing.*;
-import java.awg.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class TableViewImpl{
     
-    private
+    private final JFrame frame;
+    private final JLabel turnolabel;
+
+    private final JPanel combPanel1;//
+    private final JPanel combPanel2;//
+    private final JPanel scartiPanel;//
+    private final JPanel manoPanel;//
+
+    private final Font baseTitleFont= new Font("Arial", Font.BOLD, 32);
+    private final Font baseLabelFont = new Font("Arial", Font.BOLD, 22);
+
+    public TabelViewImpl(){
+        frame= new JFrame ("Burraco - OOOP Project");
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        frame.setLayout(new BorderLayout());
+    }
 }
