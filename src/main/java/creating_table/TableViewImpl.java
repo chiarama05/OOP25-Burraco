8fc7df2f994b6b7b813b92571d2ff254c041ba7e
Giorgia Ceccaroni <giorgia.ceccaroni@studio.unibo.it>
2026-02-26 21:11:13 +0100

    update3 to TableViewImpl

--- src/main/java/creating_table/TableViewImpl.java
+++ src/main/java/creating_table/TableViewImpl.java
@@ -6,60 +6,60 @@ import java.awt.event.ComponentEvent;
 public class TableViewImpl{
     
     private final JFrame frame;
-    private final JLabel turnoLabel;
+    private final JLabel turnLabel;
 
     private final JPanel combPanel1;//
     private final JPanel combPanel2;//
-    private final JPanel scartiPanel;//
-    private final JPanel manoPanel;//
+    private final JPanel discardPanel;//
+    private final JPanel deckPanel;//
 
     private final Font baseTitleFont= new Font("Arial", Font.BOLD, 32);
     private final Font baseLabelFont = new Font("Arial", Font.BOLD, 22);
 
-    public TabelViewImpl(){
+    public TableViewImpl(){
         frame= new JFrame ("Burraco - OOOP Project");
         frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
         frame.setLayout(new BorderLayout());
 
         //turno
-        turnoLabel = new JLabel("Turno: Giocatore1");
-        turnoLabel.setFont(baseTitleFont);
-        turnoLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,10));
-        frame.add(turnoLabel,BorderLayout.NORTH);
+        turnLabel = new JLabel("Turn: Player 1");
+        turnLabel.setFont(baseTitleFont);
+        turnLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,10));
+        frame.add(turnLabel,BorderLayout.NORTH);
 
         //combinazioni
-        JPanel combinazioniPanel = new JPanel(new GridLayout(1,2,20,10));
+        JPanel combinationPanel = new JPanel(new GridLayout(1,2,20,10));
 
         combPanel1=new JPanel(new FlowLayout(FlowLayout.LEFT,10,10));
-        combPanel1.setBorder(BorderFactory.createTitledBorder("Giocatore 1"));
+        combPanel1.setBorder(BorderFactory.createTitledBorder("Player 1"));
 
         JScrollPane scroll1=new JScrollPane(combPanel1);
         scroll1.setBorder(BorderFactory.createEmptyBorder());
-        combinazioniPanel.add(scroll1);
+        combinationPanel.add(scroll1);
 
         combPanel2=new JPanel(new FlowLayout(FlowLayout.LEFT,10,10));
-        combPanel2.setBorder(BorderFactory.createTitledBorder("Giocatore 2"));
+        combPanel2.setBorder(BorderFactory.createTitledBorder("Player 2"));
         JScrollPane scroll2 = new JScrollPane(combPanel2);
         scroll2.setBorder(BorderFactory.createEmptyBorder());
-        combinazioniPanel.add(scroll2);
+        combinationPanel.add(scroll2);
 
-        frame.add(combinazioniPanel, BorderLayout.CENTER);
+        frame.add(combinationPanel, BorderLayout.CENTER);
 
         //scarti
-        scartiPanel=new JPanel (new FlowLayout(FlowLayout.LEFT,5,5));
-        scartiPanel.setBorder(BorderFactory.createTitledBorder("Scarti"));
-        scartiPanel.setBackground(new Color (250,250,240));
+        discardPanel=new JPanel (new FlowLayout(FlowLayout.LEFT,5,5));
+        discardPanel.setBorder(BorderFactory.createTitledBorder("Discard Pile"));
+        discardPanel.setBackground(new Color (250,250,240));
 
 
         //mano
-        manoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
-        manoPanel.setBorder(BorderFactory.createTitledBorder("Mano"));
+        deckPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
+        deckPanel.setBorder(BorderFactory.createTitledBorder("Deck"));
 
 
         //contenitore inferiore
         JPanel bottomPanel = new JPanel(new BorderLayout());
-        bottomPanel.add(scartiPanel,BorderLayout.NORTH);
-        bottomPanel.add(manoPanel,BorderLayout.CENTER);
+        bottomPanel.add(discardPanel,BorderLayout.NORTH);
+        bottomPanel.add(deckPanel,BorderLayout.CENTER);
         frame.add(bottomPanel,BorderLayout.SOUTH);
 
 
@@ -79,5 +79,27 @@ public class TableViewImpl{
         frame.setVisible(true);
         applyResponsiveFonts(); 
 
+    }
+
+
+    //gestione giocatori
+    public void refreshTurnLabel(boolean turnoGiocatore1){
+        turnLabel.setText("Turn: "+(turnoGiocatore1 ? "Player 1" : "Player 2"));
+        frame.revalidate();
+        frame.repaint();
+    }
+
+
+    //gestione pozzetti
+    public void showPotFly(){
+        JOptionPane.showMessageDialog(frame,"You close your hand on 'fly', you can continue to play in this same turn!");
+    }
+
+    public void showPotnextTurn(){
+        JOptionPane.showMessageDialog(frame, "You can take your pot! You can play it in the NEXT turn!");
+    }
+
+    public void showNotValideClosure(){
+        JOptionPane.showMessageDialog(frame,"You can't discard your last card without even done a Burraco!");
     }
 }
