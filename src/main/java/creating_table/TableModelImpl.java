30d27c7b6ce83471a69e4a55c6a5f917852428ce
Giorgia Ceccaroni <giorgia.ceccaroni@studio.unibo.it>
2026-02-26 14:55:21 +0100

    updated TableModelImpl

--- src/main/java/creating_table/TableModelImpl.java
+++ src/main/java/creating_table/TableModelImpl.java
@@ -2,29 +2,76 @@ package creating_table;
 
 import java.util.*;
 
-import model.*;
-import model.card.Card;
-import model.deck.Deck;
-import model.deck.DeckImpl;
+import model.card.*;
 import model.player.*;
 
 public class TableModelImpl{
 
-    private Player player1;
-    private Player player2;
-    private Deck deck;
-    private List<Card> discardPile;
+    private final Player player1;
+    private final Player player2;
 
-    private boolean player1Turn = true;
+
+    private boolean isPlayer1Turn = true;
     private boolean haspicked = false;
+    private boolean gameFinished=false;
 
-    
-    public TableModelImpl() {
-        deck = new DeckImpl();
-        discardPile = new ArrayList<>();
-        player1 = new PlayerImpl();
-        player2 = new PlayerImpl();
+   
+    //private Winner winner = Winner.NONE;
+
+
+
+    /*public TableModelImpl(){
+        this(PlayerImpl());
+    }*/
+
+    public TableModelImpl(Player p1, Player p2){
+        this.player1=p1;
+        this.player2=p2;
     }
+
+
+    //GETTER
+    public Player getPlayer1() {
+        return player1; 
+    }
+
+    public Player getPlayer2() {
+        return player2; 
+    }
+
+    public Player getCurrentPlayer() {
+        return isPlayer1Turn ? player1 : player2;
+    }
+
+    public Player getOpponent() {
+        return isPlayer1Turn ? player2 : player1;
+    }
+
+    public boolean isPlayer1Turn() { 
+        return isPlayer1Turn;
+    }
+
+    public boolean isGameFinished() {
+        return gameFinished; 
+    }
+
+    /*public Winner getWinner() {
+        return winner; 
+    }*/
+
+    public void switchTurn() {
+        if (gameFinished) return;
+
+        isPlayer1Turn = !isPlayer1Turn;
+
+        Player newCurrent = getCurrentPlayer();
+
+        // If pozzetto must be taken, do it now.
+        if (!newCurrent.isInPot() && newCurrent.hasFinishedCards()) {
+            //newCurrent.takePozzetto(); //
+        }
+    }
+
 }
 
    
