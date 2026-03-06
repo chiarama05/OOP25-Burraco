1b37f67c4f48678f5952b9ba1fdac6f0a06179e9
Sofia <sofia.mascioli2@studio.unibo.it>
2026-03-06 15:22:58 +0100

    wildcards changes

--- src/main/java/view/table/TableViewImpl.java
+++ src/main/java/view/table/TableViewImpl.java
@@ -279,7 +279,28 @@ public class TableViewImpl implements TableView {
     public void addCombinationToPlayerPanel(List<Card> cards, boolean player1Turn) {
     JPanel targetPanel = player1Turn ? combPanel1 : combPanel2;
 
-    AttachedButton comboBtn = new AttachedButton(cards, this);
+    List<Card> orderedCards = new ArrayList<>(cards);
+
+    // Controlla se c'è un jolly
+    int jokerIndex = -1;
+    for (int i = 0; i < orderedCards.size(); i++) {
+        if (orderedCards.get(i).getValue().equals("Jolly")) {
+            jokerIndex = i;
+            break;
+        }
+    }
+
+    if (jokerIndex != -1) {
+        boolean jokerAtExtremes =
+                jokerIndex == 0 || jokerIndex == orderedCards.size() - 1;
+
+        if (jokerAtExtremes) {
+            Card joker = orderedCards.remove(jokerIndex);
+            orderedCards.add(joker); // lo spostiamo in fondo
+        }
+    }
+
+    AttachedButton comboBtn = new AttachedButton(orderedCards, this);
 
     targetPanel.add(comboBtn);
     targetPanel.revalidate();
@@ -287,6 +308,6 @@ public class TableViewImpl implements TableView {
     }
 
     public SelectionCardManager getSelectionManager() {
-    return this.selectionManager;
-}
+    return selectionManager;
+    }
 }
