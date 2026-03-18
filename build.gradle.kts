e5fe419405f4a8187282cc72bfc4f4cd33c5e13e
chiarama05 <chiara.mazzoli9@studio.unibo.it>
2026-03-18 18:48:21 +0100

    Add application plugin and configure Java toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,25 @@
-plugins {java}
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) 
+    }
+}
+
+application {
+    mainClass.set("BurracoApp")
+}
+
+tasks.processResources {
+    from("src/main/resources")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "BurracoApp"
+    }
+}
