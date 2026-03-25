77ae6c89010792665b5725fae9110bdaa0ffbe40
Chiara <chiara.mazzoli9@studio.unibo.it>
2026-03-25 14:45:45 +0100

    fixed selection notifications

---
+++ src/main/java/it/unibo/burraco/view/notification/selection/SelectionNotifierImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.burraco.view.notification.selection;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class SelectionNotifierImpl implements SelectionNotifier{
+
+    private final JFrame parent;
+
+    public SelectionNotifierImpl(JFrame parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void notifySelectionError(String errorCode) {
+        String message = switch (errorCode) {
+            case "EMPTY_SELECTION" -> "Select card from your hand first!";
+            case "INVALID_COMBINATION" -> "Combination not valid!";
+            default -> "Error.";
+        };
+
+        JOptionPane.showMessageDialog(parent, message, "Selection error", JOptionPane.WARNING_MESSAGE);
+    }
+
+}
