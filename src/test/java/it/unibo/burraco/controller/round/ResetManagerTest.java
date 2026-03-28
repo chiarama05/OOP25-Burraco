302cddbb29b684211c0c6df759a237c2cdab817e
Chiara <chiara.mazzoli9@studio.unibo.it>
2026-03-28 14:21:17 +0100

    added comments and test of ResetManager

---
+++ src/test/java/it/unibo/burraco/controller/round/ResetManagerTest.java
@@ -0,0 +1,36 @@
+package it.unibo.burraco.controller.round;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import it.unibo.burraco.model.deck.Deck;
+import it.unibo.burraco.model.discard.DiscardPile;
+import it.unibo.burraco.model.player.Player;
+
+class ResetManagerTest {
+
+    private ResetManager resetManager;
+    private Player p1;
+    private Player p2;
+    private Deck deck;
+    private DiscardPile discardPile;
+
+    @BeforeEach
+    void setUp() {
+        resetManager = new ResetManagerImpl();
+        p1 = mock(Player.class);
+        p2 = mock(Player.class);
+        deck = mock(Deck.class);
+        discardPile = mock(DiscardPile.class);
+    }
+
+    @Test
+    void testResetRoundCallsAllModels() {
+        resetManager.resetRound(p1, p2, deck, discardPile);
+        
+        verify(p1, times(1)).resetForNewRound();
+        verify(p2, times(1)).resetForNewRound();
+        verify(deck, times(1)).reset();
+        verify(discardPile, times(1)).reset();
+    }
+}
